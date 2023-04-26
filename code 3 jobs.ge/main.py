c8008161fb522d0a52fa2a463c7dc473556705ee
BeqaShevardenidze <beqa.shevardenidze968@gmail.com>
2023-04-26 12:03:48 +0400

    add file mymarket, added in jobs.ge company name

--- code 3 jobs.ge/main.py
+++ code 3 jobs.ge/main.py
@@ -25,5 +25,14 @@ for i in range(len(name)):
     print(str(name[i]))
     result = open("result.txt", "a", encoding="utf-8")
     result.write("პოზიცია = " + str(name[i].text) + "\n")
-    result.write("ლინკი = " + "https://jobs.ge" + str(name[i].get("href")) + ("\n"*2))
+    result.write("ლინკი = " + "https://jobs.ge" + str(name[i].get("href")) + "\n")
+
+    url_2 = "https://jobs.ge" + str(name[i].get("href"))
+    response_2 = requests.get(url_2)
+    soup_2 = BeautifulSoup(response_2.text, "lxml")
+    recrutor = soup_2.find_all("td", class_="dtitle")
+    recrutor_span = recrutor[1].find("span").text
+    recrutor_a_b = recrutor[1].find("b").text.replace("\n","")
+    result.write(str(recrutor_span + recrutor_a_b.replace("	"," ") + ("\n"*2)))
+
     result.close()
---
+++ code 4 mymarket/main.py
@@ -0,0 +1,19 @@
+import os
+
+def Cls():
+    os.system("cls")
+Cls()
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.mymarket.ge/ka/search/69/teqnika/mobiluri-kavshirgabmuloba/mobiluri-telefoni/?Brands=90&CatID=69&Page=1&SortID=4"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+
+data = soup.find("div").text
+
+# for i in range(len(data)):
+    # product = data.find("a", class_="h100")
